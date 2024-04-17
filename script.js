f74ac71960a4ccf67f1d5b8d0b2b28178a04faa1
Isak Hemmingsson <isak.hemmingsson@skola.taby.se>
2024-04-17 15:05:44 +0200

    17 april

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let colorArrayFire = [
+let colorArrayRedFire = [
     '#ff0000',
     '#ff5a00',
     '#ff9a00',
@@ -43,20 +43,28 @@ let colorArrayFire = [
     '#ffe808'
 ]
 
-function Flame(x, y, r, velX, velY, color) {
+let colorArrayBlueFire = [
+    '#3d88ff',
+    '#56b6ff',
+    '#50c4ff',
+    '#5bcfff',
+    '#5ad6ff'
+]
+
+function Flame(x, y, radius, velX, velY, color) {
     this.x = x
     this.y = y
-    this.r = r
+    this.r = radius
     this.vx = velX
     this.vy = velY
-    this.c = color
+    this.color = color
 
     this.draw = function() {
         c.beginPath()
         c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false)
-        c.shadowBlur = 5
-        c.shadowColor = this.c
-        c.fillStyle = this.c
+        c.shadowBlur = 15
+        c.shadowColor = this.color
+        c.fillStyle = this.color
         c.closePath()
         c.fill()
     }
@@ -76,18 +84,27 @@ function RocketProjectile(x, y, r, vel) {
     this.y = y
     this.r = r
     this.vel = vel
-    this.c = 'white'
+    this.color = 'white'
     this.draw = function() {
         c.beginPath()
         c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false)
-        c.shadowBlur = 5
-        c.shadowColor = this.c
-        c.fillStyle = this.c
+        c.shadowBlur = 25
+        c.shadowColor = this.color
+        c.fillStyle = this.color
         c.closePath()
         c.fill()
     }
 
-    this.update = function() {
+    this.update = function(asteroids) {
+        for(let i = 0; i < asteroids.length; i++) {
+            if (asteroids[i].x + asteroids[i].r > this.x &&
+                asteroids[i].x - asteroids[i].r < this.x &&
+                asteroids[i].y + asteroids[i].r > this.y &&
+                asteroids[i].y - asteroids[i].r < this.y)
+            {
+                asteroids[i].explode()
+            }
+        }
         this.y += this.vel
         this.draw()
     }
@@ -100,8 +117,9 @@ class Rocket {
         this.width = width
         this.height = height
         this.vel = vel
+        this.color = '#9171f8'
         this.health = health
-        this.healthText = createText(10, 80, this.health)
+        this.healthText = createText(this.health, innerWidth * 0.02, innerHeight * 0.95)
         this.movement = [false, false]
         this.projectileObjects = []
         this.fireObjects = []
@@ -111,21 +129,21 @@ class Rocket {
         this.projectileObjects.push(new RocketProjectile(this.x + this.width/2, this.y, 4, -9))
     }
 
-    draw() {
+    draw(asteroids) {
         c.beginPath()
         c.roundRect(this.x, this.y, this.width, this.height, 30)
-        c.fillStyle = '#9171f8'
-        c.shadowBlur = '5'
-        c.shadowColor = '#9171f8'
+        c.fillStyle = this.color
+        c.shadowBlur = 15
+        c.shadowColor = this.color
         c.closePath()
         c.fill()
         for(let i = 0; i < this.fireObjects.length; i++) {
             this.fireObjects[i].update()
         }
         for(let i = 0; i < this.projectileObjects.length; i++) {
-            this.projectileObjects[i].update()
+            this.projectileObjects[i].update(asteroids)
         }
-        this.healthText.update()
+        this.healthText.update(this.health)
     }
 
     update(asteroids) {
@@ -145,8 +163,13 @@ class Rocket {
         }
 
         for(let i = 0; i < asteroids.length; i++) {
-            if(this.x && this.y == asteroids[i].x && asteroids[i].y) {
+            if (asteroids[i].x + asteroids[i].r > this.x &&
+                asteroids[i].x - asteroids[i].r < this.x &&
+                asteroids[i].y + asteroids[i].r > this.y &&
+                asteroids[i].y - asteroids[i].r < this.y)
+            {
                 this.health -= 10
+                asteroids[i].explode()
             }
         }
 
@@ -157,10 +180,10 @@ class Rocket {
                 (Math.random() * 5) + 2,
                 (Math.random() - 0.5) * 1,
                 (Math.random()) * 5,
-                colorArrayFire[randomInt(0, 5)]
+                colorArrayRedFire[randomInt(0, 4)]
             ))
 
-        this.draw()
+        this.draw(asteroids)
     }
 }
 
@@ -169,26 +192,68 @@ function Asteroid(x, y, radius, vel) {
     this.y = y
     this.r = radius
     this.vel = vel
+    this.color = '#5bcfff'
+    this.shadowColor = '#5ad6ff'
+    this.exploded = false
+    this.explosionObjects = []
+
+    this.explode = function() {
+        this.exploded = true
+        if (this.explosionObjects.length === 0) {
+            for (let i = 0; i < randomInt(25, 30); i++) {
+                this.explosionObjects.push(
+                    new Flame(
+                        randomInt(this.x-this.r, this.x+this.r),
+                        randomInt(this.y-this.r, this.y+this.r),
+                        randomInt(4,  7),
+                        randomInt(-5, 5),
+                        randomInt(-5, 5),
+                        colorArrayBlueFire[randomInt(0, 4)]
+                    ))
+            }
+        }
+        let finishedExploding = false
+        for (let i = 0; i < this.explosionObjects.length; i++) {
+            this.explosionObjects[i].update()
+            if (this.explosionObjects[i].r < 0.51) {
+                 finishedExploding = true
+            } else {
+                finishedExploding = false
+            }
+        }
+        if (finishedExploding === true) {
+            this.reset()
+        }
+        
+    }
+
+    this.reset = function() {
+        this.y = -this.r
+        this.x = randomInt(0, canvas.width - 100)
+        this.r = randomInt(25, 50)
+        this.vel = randomInt(2, 12)
+    }
 
     this.draw = function() {
         c.beginPath()
         c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false)
-        c.fillStyle = '#808080'
-        c.shadowBlur = 5
-        c.shadowColor = '#808080'
+        c.fillStyle = this.color
+        c.shadowBlur = 12
+        c.shadowColor = this.shadowColor
         c.closePath()
         c.fill()
     }
 
     this.update = function() {
-        this.y += this.vel
-        if(this.y >= canvas.height) {
-            this.y = -this.r
-            this.x = randomInt(0, canvas.width - 100)
-            this.r = randomInt(25, 50)
-            this.vel = randomInt(2, 12)
+        if (this.exploded === false) {
+            this.y += this.vel
+            if(this.y >= canvas.height + this.r) {
+                this.reset()
+            }
+            this.draw()
+        } else {
+            this.explode()
         }
-        this.draw()
     }
 }
 
@@ -198,33 +263,37 @@ function createRocket() {
     let width = 25
     let height = width * 2
     let velocity = 10
-    let health = 3
+    let health = 100
     return new Rocket(x, y, width, height, velocity, health)
 }
 
-function createAsteroids() {
-    let asteroidCount = 9
+function createAsteroids(count) {
+    let asteroidCount = count
     for (let i = 0; i < asteroidCount; i++) {
         let x = randomInt(0, canvas.width - 100)
-        let y = 10
+        const y = 10
         let radius = randomInt(25, 50)
         let velocity = randomInt(2, 12)
         asteroidArray.push(new Asteroid(x, y, radius, velocity))
     }
 }
 
-function createText(x, y, text) {
-    return new Text(x, y, text.toString())
+function createText(text, x, y) {
+    return new Text(text, x, y)
 }
 
 class Text {
-    constructor(x, y, text) {
+    constructor(text, x, y) {
         this.x = x
         this.y = y
         this.txt = text
+        this.color = 'red'
     }
 
     draw() {
+        c.shadowBlur = 5
+        c.shadowColor = this.color
+        c.fillStyle = this.color
         c.font = '50px Arial'
         c.fillText(this.txt, this.x, this.y)
     }
@@ -237,7 +306,7 @@ class Text {
 
 function initialize() {
     rocket = createRocket()
-    createAsteroids()
+    createAsteroids(9)
     createText(10, 80, 100)
     animate()
 }
